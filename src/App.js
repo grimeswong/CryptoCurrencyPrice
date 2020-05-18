286995d3141118639cd154fe91a60ff9a003017a
Shadow Grimes <grimeswongatwork@gmail.com>
2020-05-18 23:50:27 +1000

    #1 - Refine order of import style sheets & test Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Create React App Base with Sass</h1>
+      <button className="alert-danger">a Bootstrap button</button>
     </div>
   );
 }
