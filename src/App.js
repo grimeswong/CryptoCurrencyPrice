99500241741108bbde80bf7fefb37fbc86024567
Shadow Grimes <grimeswongatwork@gmail.com>
2020-05-19 18:11:31 +1000

    #1 - Convert App to class component

--- src/App.js
+++ src/App.js
@@ -1,58 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './styles/main.scss';
 
-function App() {
-  return (
-    <main className="App">
-      <div className="container">
-        <h1>Cryptocurrency Price</h1>
+class App extends Component {
 
-        <div className="section selection-wrapper">
-          <div className="search-wrapper col-sm-6">
-            <input className="form-control" type="text" placeholder="Search" aria-label="Search" />
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataLoaded: false
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return(
+      <main className="App">
+        <div className="container">
+          <h1>Cryptocurrency Price</h1>
+
+          <div className="section selection-wrapper">
+            <div className="search-wrapper col-sm-6">
+              <input className="form-control" type="text" placeholder="Search" aria-label="Search" />
+            </div>
+            <div className="form-wrapper col-sm-6">
+              <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="radio" id="change" value="option1" checked/>
+                <label className="form-check-label" htmlFor="change">
+                  Change
+                </label>
+              </div>
+              <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="radio" id="volume" value="option2" />
+                <label className="form-check-label" htmlFor="volumn">
+                  Volume
+                </label>
+              </div>
+            </div>
           </div>
-          <div className="form-wrapper col-sm-6">
-            <div className="form-check form-check-inline">
-              <input className="form-check-input" type="radio" name="radio" id="change" value="option1" checked/>
-              <label className="form-check-label" htmlFor="change">
-                Change
-              </label>
-            </div>
-            <div className="form-check form-check-inline">
-              <input className="form-check-input" type="radio" name="radio" id="volume" value="option2" />
-              <label className="form-check-label" htmlFor="volumn">
-                Volume
-              </label>
-            </div>
+
+          <div className="section list-wrapper">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">Pair</th>
+                  <th scope="col">Last Price</th>
+                  <th scope="col">Change</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>ADA/BTC</td>
+                  <td>0.00000617</td>
+                  <td>-0.64</td>
+                </tr>
+                <tr>
+                  <td>ADX/BTC</td>
+                  <td>0.00001012</td>
+                  <td>+1.10</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
+      </main>
+    )
+  }
 
-        <div className="section list-wrapper">
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">Pair</th>
-                <th scope="col">Last Price</th>
-                <th scope="col">Change</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>ADA/BTC</td>
-                <td>0.00000617</td>
-                <td>-0.64</td>
-              </tr>
-              <tr>
-                <td>ADX/BTC</td>
-                <td>0.00001012</td>
-                <td>+1.10</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </main>
-  );
 }
 
 export default App;
