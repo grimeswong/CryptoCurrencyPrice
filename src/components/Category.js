35e21b9ce603062d2cc008379ebd8129c0e76c3d
Shadow Grimes <grimeswongatwork@gmail.com>
2020-05-22 02:25:57 +1000

    #1 - Implement the sorted list for displaying the catagories

--- src/components/Category.js
+++ src/components/Category.js
@@ -2,18 +2,41 @@ import React from 'react';
 
 const Category = (props) => {
 
-  // to do: use filter function
-  // if the current quote is same as the parent market, ignore it
-  const listCategories = props.data.map((element) => {
-    return (
-      <li>{`${element.pm}`}</li>
-    )
-  })
+  // props.data.map((element)=> console.log(`quote=${element.q}, pm=${element.pm}`)); //debugger
 
+  // Process the Category list with groups
+  const types = {};
+  for (let i = 0; i < props.data.length; i++) {
+    let typeName = props.data[i].pm;
+    if (!types[typeName]) {
+        types[typeName] = [];
+    }
+    types[typeName].push(props.data[i].q);
+  }
+
+  console.log(types); //debugger
+
+  // Remove duplicate elements in group
+  const sortedCategories = [];
+  for (const type in types) {
+    let groups = types[type].filter((item, index) => {
+      return types[type].indexOf(item) === index;
+    });
+    sortedCategories.push({Category: type, symbols: groups});
+  }
+
+  console.log(sortedCategories); //debugger
+
+  // Load the Category list and with button groups
+  // const listCategories = types.sort().map((element) => {
+  //   return (
+  //     <li key={element}>{`${element}`}</li>
+  //   )
+  // });
 
   return (
     <ul>
-      {listCategories}
+      {}
     </ul>
   )
 }
