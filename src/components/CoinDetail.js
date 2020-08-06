25c98b2bb66c20050e9575383bab954e3141cff7
grimeswong <grimeswongatwork@gmail.com>
2020-08-06 17:11:39 +1000

    #4 - Change the colour of difference of currency price

--- src/components/CoinDetail.js
+++ src/components/CoinDetail.js
@@ -5,9 +5,13 @@ import limitDigit from './limitDigit.js';
 
 const CoinDetail = (props) => {
 
+  /* Dynamic styling value */
+  const changeClass = parseFloat(((props.element.c - props.element.o) / props.element.o) * 100).toFixed(2) >= 0 ? "positive" : "negative";
+  const lastpriceClass = props.element.c-props.element.o > 0 ? "positive" : "negative";
+
   const radio = props.radioSelection==='option1' ?
   /* Fixed in 2 decimal number with plus or minus sign prefix comparing with open price */
-  <td className={parseFloat(((props.element.c - props.element.o) / props.element.o) * 100).toFixed(2) >= 0 ? "positive" : "negative"}>
+  <td className={changeClass}>
     {`${parseFloat(((props.element.c - props.element.o) / props.element.o) * 100).toFixed(2) >= 0 ? "+" : ""}${parseFloat(((props.element.c - props.element.o) / props.element.o) * 100).toFixed(2)}%`}
   </td> :
   /* Fixed in no decimal number */
@@ -17,8 +21,8 @@ const CoinDetail = (props) => {
   return(
     <tr key={props.element.s}>
       <td><span className="coinDetail-base">{`${props.element.b}`}</span><span className="coinDetail-quote">/{`${props.element.q}`}</span></td>
-      <td>{limitDigit((props.element.c),8)}</td>
       <td>{limitDigit((props.element.o),8)}</td>
+      <td className={lastpriceClass}>{limitDigit((props.element.c),8)}</td>
       {radio}
     </tr>
   )
