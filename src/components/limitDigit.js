33faaa9fbadd418835627f92ddbb4610605b39e9
grimeswong <grimeswongatwork@gmail.com>
2020-08-24 11:45:17 +1000

    #11 - Modify comments of limitDigit function

--- src/components/limitDigit.js
+++ src/components/limitDigit.js
@@ -1,5 +1,9 @@
-// function to limit to 8 decimal points and remove unused zero after decimal point
-// this fucntion allows the type of string and number as argument
+/**
+ *   A function to limit to 8 decimal points and remove unused zero after decimal point
+ *   arguments: (1st arg) - allows the type of string and number as argument, (2nd arg) - number of desired digits
+ *   return: a string of converted number 
+ **/
+
 
 const limitDigit = (num, fixedDigit) => {
     let fixedNum, temp;
